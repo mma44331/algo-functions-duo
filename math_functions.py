ec8ff83de9f24cf79f76adbc9927a85bddf5c152
meyir <mma088651259@gmai.com>
2025-06-19 16:56:16 +0300

    הוסף פונקציית בדיקת מספר ראשוני

--- math_functions.py
+++ math_functions.py
@@ -3,7 +3,18 @@
 
 
 def wlcome():
-    # ונקציה לברכה
+     # ונקציה לברכה
     print("ברוכים הבאים לספרייה של פונקציות אלורתמיות")
+
+def is_prime(target):
+    if target<2:
+        return False
+    for i in range(2,int(target/2)+1):
+        if (target%i)==0:
+            return False
+    return True
+
+
 if __name__=="__main__":
     wlcome()
+    print(is_prime((15)))
