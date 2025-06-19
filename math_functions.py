4f5fcbe4c12968961a1ec15b48f0c9b10c29bb9b
Hillel <hillelsuied@gmail.com>
2025-06-19 16:17:23 +0300

    הוספת פונקציית פקטוריאל (עצרת)

--- math_functions.py
+++ math_functions.py
@@ -5,5 +5,17 @@
 def wlcome():
     # ונקציה לברכה
     print("ברוכים הבאים לספרייה של פונקציות אלורתמיות")
+
+def factorial(n):
+    """מחזירה את הפקטוריאל של מספר שלם n"""
+    if n < 0:
+        raise ValueError("לא ניתן לחשב פקטוריאל למספר שלילי")
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)    
+
+
+
 if __name__=="__main__":
     wlcome()
+    print(factorial(5))  # פלט צפוי: 120
